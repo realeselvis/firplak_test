6186f454f5f93ed7602105782341afc8a6fa86fe
Reales Elvis <realeselvis3@gmail.com>
2024-03-10 21:45:56 -0500

    Se agrega logout

--- js/script.js
+++ js/script.js
@@ -4,29 +4,35 @@ console.log(nombreUsuario);
 
 header.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light p-2">
+
         <a class="navbar-brand" href="https://www.firplak.com/" target="_blank">
-            <img src="img/logo_firplak.webp" alt="Logo" height="40"> <!-- Ajusta la altura según tus necesidades -->
+            <img src="img/logo_firplak.webp" alt="Logo" height="40">
         </a>
+        <div class="d-flex align-items-center ms-auto border border-primary rounded me-2">
+            <p class="text-end py-1 px-1 mb-0">👋 ¡Hola <strong>${nombreUsuario}</strong>!</p>
+        </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/firplak">Inicio</a>
                 </li>
-                <!-- <li class="nav-item">
-                    <a class="nav-link" href="#">Transportadores</a>
-                </li> -->
                 <li class="nav-item">
                     <a class="nav-link" href="https://realeelvis.online/" target="_blank">Contacto</a>
-                </li>       
+                </li>
             </ul>
+            <div class="d-flex">
+                <a href="logout.php" class="btn btn-danger">Cerrar sesión</a>
+            </div>
         </div>
-    </nav>
-    <p class="text-end pt-1 px-1">👋 ¡Hola ${nombreUsuario}!</p>
+        <!-- Mensaje de bienvenida fuera del div colapsable pero dentro del contenedor nav -->
 
+    </nav>
 `;
+
+
 
 footer.innerHTML = `
     <p>&copy; 2024 Firplak. Todos los derechos reservados.</p>
